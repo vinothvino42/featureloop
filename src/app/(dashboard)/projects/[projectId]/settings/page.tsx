0679a1cd92448118d6232012d394b355ed302b0b
vinothvino42 <vinothvino42@gmail.com>
2025-07-07 22:24:24 +0530

    feat(settings): add setting route

---
+++ src/app/(dashboard)/projects/[projectId]/settings/page.tsx
@@ -0,0 +1,13 @@
+export default function SettingPage() {
+  return (
+    <div>
+      ProjectSettingPage: Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Ex tempore officiis quis sunt debitis excepturi, sit voluptas
+      veritatis provident minima dolorem perspiciatis placeat, distinctio et
+      porro ipsum qui, eaque eligendi! Ipsum ad recusandae praesentium veritatis
+      corrupti sequi neque, excepturi vero reprehenderit totam eum, similique
+      consectetur laudantium dolorum voluptatum voluptates rem. Harum hic
+      debitis rerum nemo, illo dignissimos placeat. Officia, vero!
+    </div>
+  );
+}
